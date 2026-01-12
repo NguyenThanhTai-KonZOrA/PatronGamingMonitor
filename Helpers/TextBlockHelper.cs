6b6e9fa52308cfb4407ef840ad1226b725055192
TaiThanh.Nguyen <nguyenthanhtai050895@gmail.com>
2026-01-12 14:24:51 +0700

    Update code.

---
+++ Helpers/TextBlockHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace PatronGamingMonitor.Helpers
+{
+    public static class TextBlockHelper
+    {
+        public static readonly DependencyProperty RemoveUnderlineProperty =
+            DependencyProperty.RegisterAttached(
+                "RemoveUnderline",
+                typeof(bool),
+                typeof(TextBlockHelper),
+                new PropertyMetadata(false, OnRemoveUnderlineChanged));
+
+        public static bool GetRemoveUnderline(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(RemoveUnderlineProperty);
+        }
+
+        public static void SetRemoveUnderline(DependencyObject obj, bool value)
+        {
+            obj.SetValue(RemoveUnderlineProperty, value);
+        }
+
+        private static void OnRemoveUnderlineChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TextBlock textBlock && (bool)e.NewValue)
+            {
+                textBlock.Loaded += (s, args) =>
+                {
+                    textBlock.TextDecorations = null;
+                };
+                textBlock.TextDecorations = null;
+            }
+        }
+    }
+}
